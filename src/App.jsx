504d58cd2f8805d5f213f5e94aa068bf4009b0d3
Nawal-ali09 <nawalali924m@gmail.com>
2025-11-10 17:47:26 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { ChatInput } from "./components/ChatInput";
+import { useState } from "react";
 
 function App() {
+  const [scriptCommand, setScriptCommand] = useState("");
+
   return (
-    <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <>
+      <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
+        <color attach="background" args={["#000000"]} />
+        <Experience scriptCommand={scriptCommand} />
+      </Canvas>
+      <ChatInput onSend={(text) => setScriptCommand(text.toLowerCase())} />
+    </>
   );
 }
 
